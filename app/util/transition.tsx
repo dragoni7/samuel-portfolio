15bd1e82c4a7dfd4fdac510818d976a74db99259
dragoni7 <sdgibson18@outlook.com>
2024-08-23 17:48:49 -0700

    Fix build errors

--- app/util/transition.tsx
+++ app/util/transition.tsx
@@ -1,23 +1,25 @@
 import { motion } from 'framer-motion';
 
-export default function transition(Component: React.FC) {
-  return () => (
-    <>
-      <Component />
-      <motion.div
-        className="slide-out fixed top-0 left-0 w-full h-full bg-yellow-500 origin-right"
-        initial={{ scaleX: 1 }}
-        animate={{ scaleX: 0 }}
-        exit={{ scaleX: 0 }}
-        transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
-      />
-      <motion.div
-        className="slide-in fixed top-0 left-0 w-full h-full bg-yellow-500 origin-left"
-        initial={{ scaleX: 1 }}
-        animate={{ scaleX: 0 }}
-        exit={{ scaleX: 0 }}
-        transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
-      />
-    </>
-  );
+export default function transition(Component: React.FC): () => JSX.Element {
+  return function Transition() {
+    return (
+      <>
+        <Component />
+        <motion.div
+          className="slide-out fixed top-0 left-0 w-full h-full bg-yellow-500 origin-right"
+          initial={{ scaleX: 1 }}
+          animate={{ scaleX: 0 }}
+          exit={{ scaleX: 0 }}
+          transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
+        />
+        <motion.div
+          className="slide-in fixed top-0 left-0 w-full h-full bg-yellow-500 origin-left"
+          initial={{ scaleX: 1 }}
+          animate={{ scaleX: 0 }}
+          exit={{ scaleX: 0 }}
+          transition={{ duration: 3, ease: [0.22, 1, 0.36, 1] }}
+        />
+      </>
+    );
+  };
 }
